cec69eb0f17d87a6fe38833347f15cb20a69d747
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-10-18 15:04:14 +0200

    Even better normalisation of changelogs

--- src/main/scala/io/github/noeppi_noeppi/tools/cfupdatechecker/curse/CurseAPI.scala
+++ src/main/scala/io/github/noeppi_noeppi/tools/cfupdatechecker/curse/CurseAPI.scala
@@ -43,6 +43,7 @@ object CurseAPI {
     val theStr = if (str.startsWith("\"") && str.endsWith("\"")) str.substring(1, str.length - 1) else str
     StringEscapeUtils.unescapeJava(theStr).replace("\r", "").replaceAll("\\n+", "\n").strip()
       .map(chr => if (chr.isWhitespace && chr != '\n') ' ' else chr).replaceAll(" +", " ")
+      .replace("\n \n", "\n").replaceAll("\\n+", "\n").strip()
   }
 
   private def query(endpoint: String): JsonElement = query(endpoint, reader => Util.GSON.fromJson(reader, classOf[JsonElement]))
